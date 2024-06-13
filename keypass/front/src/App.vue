fa1f868fdc0b8d847b2a992accc512172b235dd3
Shiro1234orihS <ricardonunesemilio@gmail.com>
2024-06-13 22:18:13 +0100

    tableau

--- keypass/front/src/App.vue
+++ keypass/front/src/App.vue
@@ -12,8 +12,12 @@ const isConnexionView = computed(() => {
 <template>
   <header v-if="isConnexionView">
     <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+    
+      <div class="menu">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </div>
+      <button class="profile-button">Profil</button>
     </nav>
   </header>
 
@@ -21,11 +25,33 @@ const isConnexionView = computed(() => {
 </template>
 
 <style scoped>
-header{
-  display: flex; 
-  justify-content: center;
-  align-items: center; 
-  height: 50px;
+header {
   background: rgb(76, 76, 212);
 }
+
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+}
+
+.menu {
+  display: flex;
+  gap: 20px;
+  flex-grow: 1;
+  justify-content: center;
+  align-items: center;
+}
+
+.profile-button {
+  width:60px ;
+  height:60px ;
+  margin-right: 20px; /* Adjust as needed */
+  border-radius: 100% ;
+  border-color:  #E74C3C;
+}
+
+
+
 </style>
--- keypass/front/src/views/HomeView.vue
+++ keypass/front/src/views/HomeView.vue
@@ -1,12 +1,40 @@
-<script>
-
-</script>
-
 <template>
-  <p>test</p>
-</template>
+    <table>
+      <tr>
+        <td>
+          <p>Nom de l'application</p>
+        </td>
+        <td>
+          <p>Liens</p>
+        </td>
+        <td>
+          <p>Nom d'utilisateur</p>
+        </td>
+        <td>
+          <p>Mots de passe</p>
+        </td>
+        <td>
+          <p>Commentaire</p>
+        </td>
+      </tr>
+      <tr>
+        <td><p>GOOGLE</p></td>
+        <td><p>GOOGLE</p></td>
+        <td><p>GOOGLE</p></td>
+        <td><p>GOOGLE</p></td>
+        <td><p>GOOGLE</p></td>
+      </tr>
+    </table>
+  </template>
   
-<style scoped>
-
-</style>
+  <style scoped>
+  table, th, td {
+    border: 1px solid rgb(255, 255, 255);
+    border-collapse: collapse;
+  }
+  
+  p {
+    color: aliceblue;
+  }
+  </style>
   
