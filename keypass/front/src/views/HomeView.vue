e98a94c7b36c6fe6ce1d8f7c81ed34cddf941855
Shiro1234orihS <ricardonunesemilio@gmail.com>
2024-06-15 22:22:47 +0100

    j'ai fais le copie coller

--- keypass/front/src/views/HomeView.vue
+++ keypass/front/src/views/HomeView.vue
@@ -1,41 +1,63 @@
 <template>
-    <table>
-      <tr>
-        <td>
-          <p>Nom de l'application</p>
-        </td>
-        <td>
-          <p>Liens</p>
-        </td>
-        <td>
-          <p>Nom d'utilisateur</p>
-        </td>
-        <td>
-          <p>Mots de passe</p>
-        </td>
-        <td>
-          <p>Commentaire</p>
-        </td>
-      </tr>
-      <tr>
-        <td><p>GOOGLE</p></td>
-        <td><p>GOOGLE</p></td>
-        <td><p>GOOGLE</p></td>
-        <td><p>GOOGLE</p></td>
-        <td><p>GOOGLE</p></td>
-      </tr>
-      
-    </table>
-  </template>
-  
-  <style scoped>
-  table, th, td {
-    border: 1px solid rgb(0, 0, 0);
-    border-collapse: collapse;
+  <table>
+    <tr>
+      <td>
+        <p>Nom de l'application</p>
+      </td>
+      <td>
+        <p>Liens</p>
+      </td>
+      <td>
+        <p>Nom d'utilisateur</p>
+      </td>
+      <td>
+        <p>Mots de passe</p>
+      </td>
+      <td>
+        <p>Commentaire</p>
+      </td>
+    </tr>
+    <tr>
+      <td><div class="placement-logo"><p>GOOGLE</p><img src="./../assets/img/copie.webp" alt="copie" id="google1" @click="copierTexte('google1')" class="copie"></div></td>
+      <td><div class="placement-logo"><p>GOOGLE</p><img src="./../assets/img/copie.webp" alt="copie" id="google2" @click="copierTexte('google2')" class="copie"></div></td>
+      <td><div class="placement-logo"><p>GOOGLE</p><img src="./../assets/img/copie.webp" alt="copie" id="google3" @click="copierTexte('google3')" class="copie"></div></td>
+      <td><div class="placement-logo"><p>GOOGLE</p><img src="./../assets/img/copie.webp" alt="copie" id="google4" @click="copierTexte('google4')" class="copie"></div></td>
+      <td><div class="placement-logo"><p>GOOGLE</p><img src="./../assets/img/copie.webp" alt="copie" id="google5" @click="copierTexte('google5')" class="copie"></div></td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  methods: {
+    copierTexte(id) {
+      const texte = document.getElementById(id).innerText;
+      navigator.clipboard.writeText(texte).then(() => {
+        alert('Texte copié avec succès!');
+      }).catch(err => {
+        console.error('Erreur lors de la copie du texte : ', err);
+      });
+    }
   }
-  
-  p {
-    color: rgb(0, 0, 0);
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+table, th, td {
+  border: 1px solid rgb(0, 0, 0);
+  border-collapse: collapse;
+}
+
+p {
+  color: rgb(0, 0, 0);
+}
+
+.placement-logo{
+  display: flex;
+}
+
+.copie{
+  height: 50px;
+  cursor: pointer; /* Ajoute un curseur pour indiquer que l'élément est cliquable */
+}
+</style>
