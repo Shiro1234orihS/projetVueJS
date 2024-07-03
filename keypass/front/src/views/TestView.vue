e8118b8565fa6d26158f58e00b65634bec5ed6aa
Shiro1234orihS <ricardonunesemilio@gmail.com>
2024-07-03 22:25:33 +0100

    j'ai rajouter des idées

--- keypass/front/src/views/TestView.vue
+++ keypass/front/src/views/TestView.vue
@@ -2,28 +2,39 @@
     <div id="containt">
         <img src="https://github.com/favicon.ico" alt="test">
         <h1>Github</h1>
+        <div>
+            <p>Copy le nom d'utilisateur</p>
+            <p>Copy le mots de passe</p>
+        </div>
     </div>
+    <div v-show="visible">
+        <div class="hoverable">
+            <p>test</p>
+            <img src="./../assets/img/copie.webp" alt="copie" class="copie">
+        </div>
 
-    <div class="hoverable">
-        <p>test</p>
-        <img src="./../assets/img/copie.webp" alt="copie" class="copie">
-    </div>
+        <div class="hoverable">
+            <p>test</p>
+            <img src="./../assets/img/copie.webp" alt="copie"class="copie">
+        </div>
 
-    <div class="hoverable">
-        <p>test</p>
-        <img src="./../assets/img/copie.webp" alt="copie"class="copie">
-    </div>
+        <div class="hoverable">
+            <p>test</p>
+            <img src="./../assets/img/copie.webp" alt="copie"  class="copie">
+        </div>
 
-    <div class="hoverable">
-        <p>test</p>
-        <img src="./../assets/img/copie.webp" alt="copie"  class="copie">
-    </div>
-
-    <div class="hoverable">
-        <p>test</p>
-        <img src="./../assets/img/copie.webp" alt="copie"  class="copie">
+        <div class="hoverable">
+            <p>test</p>
+            <img src="./../assets/img/copie.webp" alt="copie"  class="copie">
+        </div>
     </div>
 </template>
+<script>
+import { ref } from 'vue';
+
+
+const visible = ref(false); 
+</script>
 
 <style scoped>
 img {
@@ -40,11 +51,13 @@ img {
 
     border: solid;
     border-radius: 20px;
+
+    transition: background-color 0.3s, border-color 0.3s;
 }
 
-#containt:hover {
+#containt:hover{
     background-color: #1b1a1a4f;
-    border-color: rgb(114, 75, 75);
+    border-color:  rgb(114, 75, 75);
 }
 
 
