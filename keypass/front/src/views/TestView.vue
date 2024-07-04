ce9f38c3e2d97adcde3fff1aceee5a3f2c8d8647
Shiro1234orihS <ricardonunesemilio@gmail.com>
2024-07-04 22:20:49 +0100

    j'ai un peu améliorer

--- keypass/front/src/views/TestView.vue
+++ keypass/front/src/views/TestView.vue
@@ -1,10 +1,14 @@
 <template>
-    <div id="containt">
-        <img src="https://github.com/favicon.ico" alt="test">
-        <h1>Github</h1>
+    <div id="containt"  @click="clickItem">
         <div>
-            <p>Copy le nom d'utilisateur</p>
-            <p>Copy le mots de passe</p>
+            <div class="duo">
+                <img src="https://github.com/favicon.ico" alt="test">
+                <h1>Github</h1>
+            </div>
+            <div class="duo">
+                <div class="duo copy"><img src="./../assets/img/copie.webp" alt="copie" class="copie"><p>Copy le nom d'utilisateur</p></div>
+                <div class="duo copy"><img src="./../assets/img/copie.webp" alt="copie" class="copie"><p>Copy le mot de passe</p></div>
+            </div>
         </div>
     </div>
     <div v-show="visible">
@@ -15,25 +19,38 @@
 
         <div class="hoverable">
             <p>test</p>
-            <img src="./../assets/img/copie.webp" alt="copie"class="copie">
+            <img src="./../assets/img/copie.webp" alt="copie" class="copie">
         </div>
 
         <div class="hoverable">
             <p>test</p>
-            <img src="./../assets/img/copie.webp" alt="copie"  class="copie">
+            <img src="./../assets/img/copie.webp" alt="copie" class="copie">
         </div>
 
         <div class="hoverable">
             <p>test</p>
-            <img src="./../assets/img/copie.webp" alt="copie"  class="copie">
+            <img src="./../assets/img/copie.webp" alt="copie" class="copie">
         </div>
     </div>
 </template>
+
 <script>
 import { ref } from 'vue';
 
+export default {
+    setup() {
+        const visible = ref(false);
 
-const visible = ref(false); 
+        const clickItem = () => {
+            visible.value = !visible.value;
+        };
+
+        return {
+            visible,
+            clickItem
+        };
+    }
+};
 </script>
 
 <style scoped>
@@ -41,24 +58,27 @@ img {
     height: 50px;
 }
 #containt {
-    height: 10%;
     width: 30%;
-
     display: flex;
     align-items: center;
-
     background-color: #4d4d4d4f;
-
     border: solid;
     border-radius: 20px;
-
     transition: background-color 0.3s, border-color 0.3s;
 }
 
-#containt:hover{
-    background-color: #1b1a1a4f;
-    border-color:  rgb(114, 75, 75);
+.duo {
+    display: flex;
 }
 
+#containt:hover {
+    background-color: #1b1a1a4f;
+    border-color: rgb(114, 75, 75);
+    cursor: pointer;
+}
 
+.copy:hover {
+    background-color: #3e5f214f;
+    border-radius: 20px;
+}
 </style>
